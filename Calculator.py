c35153754d84a26a3a6419ae5dbb8cf1b3f9aa28
Melvin James <123574144+melvinjames1@users.noreply.github.com>
2024-11-21 23:52:36 +0530

    Update Calculator.py
    added power off feature

--- Calculator.py
+++ Calculator.py
@@ -13,6 +13,9 @@ def multiply(a, b):
 # This function divides two numbers
 def divide(a, b):
     return a / b
+#function forpowero
+def pow(a, b):
+    return a ** b
 
 
 print("Select operation.")
@@ -20,13 +23,14 @@ print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Power")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4','5'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -45,6 +49,9 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
+       
+        elif choice == '5':
+            print(num1, "to the power of", num2, "=", pow(num1, num2))
         
         # check if user wants another calculation
         # break the while loop if answer is no
